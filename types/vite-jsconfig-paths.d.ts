a1b342af6d62803e839da3192d36dc173e250a24
AnilBASARAN <nlbsrn@gmail.com>
2024-12-13 00:27:23 +0300

    final

--- types/vite-jsconfig-paths.d.ts
+++ types/vite-jsconfig-paths.d.ts
@@ -1,6 +1,6 @@
 declare module 'vite-jsconfig-paths' {
     import { Plugin } from 'vite';
-    const plugin: () => Plugin;
-    export default plugin;
+    const jsconfigPaths: () => Plugin;
+    export default jsconfigPaths;
   }
   
